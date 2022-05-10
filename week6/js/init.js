9a49e7b8b0a829f0be466fe7f8c3446c9ccf068b
kris10fu <kristen.fu88@gmail.com>
2022-05-10 16:46:03 -0700

    Finished Lab 6

--- week6/js/init.js
+++ week6/js/init.js
@@ -1,5 +1,5 @@
 // declare variables
-let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+let mapOptions = {'center': [34.052235,-118.243683],'zoom':5}
 
 // use the variables
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
@@ -9,29 +9,27 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // create a function to add markers
-function addMarker(data){
-    console.log(data)
-    L.marker([data.lat,data.lng]).addTo(map).bindPopup(`<h2>${data.Location}</h2> <h3>${data.OpenEnded}</h3>`)
-    createButtons(data)
-    return
+function addMarker(lat,lng,Location,message,message2){
+    console.log(message)
+    L.circleMarker([lat,lng]).addTo(map).bindPopup(`<h2>${Location}</h2> <h3>${message}</h3> <h3>${message2}</h3>`)
+    createButtons(lat,lng,Location)
+    return message
 }
 
-function createButtons(data){
+function createButtons(lat,lng,title){
     const newButton = document.createElement("button"); // adds a new button
-    newButton.id = "button"+data.Location; // gives the button a unique id
-    newButton.innerHTML = data.Location; // gives the button a title
-    newButton.setAttribute("lat",data.lat); // sets the latitude 
-    newButton.setAttribute("lng",data.lng); // sets the longitude 
+    newButton.id = "button"+title; // gives the button a unique id
+    newButton.innerHTML = title; // gives the button a title
+    newButton.setAttribute("lat",lat); // sets the latitude 
+    newButton.setAttribute("lng",lng); // sets the longitude 
     newButton.addEventListener('click', function(){
-        map.flyTo([data.lat,data.lng]); //this is the flyTo from Leaflet
+        map.flyTo([lat,lng]); //this is the flyTo from Leaflet
     })
-    document.body.appendChild(newButton); //this adds the button to our page.
-    const spaceForButtons = document.getElementById('placeForButtons')
+    const spaceForButtons = document.getElementById('placeForButtons'); //this adds the button to our page.
     spaceForButtons.appendChild(newButton);
 }
 
-
-const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS2WyfKTyZJ-_ja3GGrxoAXwranavyDGXYsxeFUO4nvHpCJrkKhChymXQqUEyhdGLnz9VN6BJv5tOjp/pub?gid=1560504149&single=true&output=csv"
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR9W4iyzco7YwMKtYcsFrMYP3Rco--qwSQuA8qKjSTHnuSz3lCKSvo-3KINAYjS4PW-HKCUeigOg85c/pub?output=csv"
 
 function loadData(url){
     Papa.parse(url, {
@@ -45,7 +43,7 @@ function processData(results){
     console.log(results)
     results.data.forEach(data => {
         console.log(data)
-        addMarker(data)
+        addMarker(data.lat,data.lng,data.Location,data.Diversity, data.Inclusion)
     })
 }
 
